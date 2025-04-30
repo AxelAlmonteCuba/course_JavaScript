996cdabd417437d074305729cfba01c076e6d610
AxelAlmonte <aalmontecu@unsa.edu.pe>
2025-04-30 12:55:31 -0500

    inicios de curso: uso de un archivo .js y datos

---
+++ main.js
@@ -0,0 +1,10 @@
+
+//alert('hola mundo')
+
+var nombre = ('Axel Almonte')
+var edad = 22
+
+//document.write('Hola ' + nombre + ' tienes ' + edad + ' años' + '<br>')
+
+var datos = document.getElementById('datos')
+datos.innerHTML = 'Hola ' + nombre + ' tienes ' + edad + ' años' + '<br>'
