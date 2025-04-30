7cbb9d3a6c601bf45509db869b79fad694af9fcc
AxelAlmonte <aalmontecu@unsa.edu.pe>
2025-04-30 14:23:35 -0500

    compilando a js

---
+++ TypeScript/main.js
@@ -0,0 +1,7 @@
+//variables
+var nombre = "Axel";
+var apellido = "Almonte";
+var edad = 22;
+//constantes
+var dni = 87654321;
+console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
---
+++ TypeScript/main.ts
@@ -0,0 +1,9 @@
+//variables
+let nombre:string = "Axel";
+let apellido:string = "Almonte";
+let edad:number = 22;
+
+//constantes
+const dni:number = 87654321;
+
+console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
