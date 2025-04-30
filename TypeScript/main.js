faf1c74fa51d10c0217d8bd11eb023a7570e3f0e
AxelAlmonte <aalmontecu@unsa.edu.pe>
2025-04-30 15:05:48 -0500

    usando funciones con condicionales de edad

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -7,9 +7,26 @@ var dni = 87654321;
 console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
 //arreglos
 var personas = ["Axel", "Almonte", "Jose"];
+var edades = [22, 11, 24];
 var div_personas = document.querySelector("#personas");
-div_personas.innerHTML = "<ul>";
-console.log(personas.map(function (persona) {
-    return "<li> ".concat(persona, " </li>");
-}).join(""));
+div_personas.innerHTML = "<ul>" +
+    personas.map(function (persona) {
+        return "<li> ".concat(persona, " </li>");
+    }).join("");
 div_personas.innerHTML += "</ul>";
+mostrar_datos(personas, edades, div_personas);
+//funciones
+function esMayorDeEdad(edad) {
+    return edad >= 18;
+}
+function mostrar_datos(nombre, edades, div) {
+    for (var i = 0; i < nombre.length; i++) {
+        div.innerHTML += "<p>".concat(nombre[i], " tiene ").concat(edades[i], " a\u00F1os</p>");
+        if (esMayorDeEdad(edades[i])) {
+            div.innerHTML += "<p>".concat(nombre[i], " es mayor de edad</p>");
+        }
+        else {
+            div.innerHTML += "<p>".concat(nombre[i], " es menor de edad</p>");
+        }
+    }
+}
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -10,13 +10,30 @@ console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " año
 
 //arreglos
 let personas:string[] = ["Axel", "Almonte", "Jose"];
+let edades:number[] = [22, 11, 24];
 
 let div_personas:HTMLElement | null = document.querySelector("#personas");
-div_personas.innerHTML = "<ul>";
-
-console.log(personas.map((persona)=>{
-    return `<li> ${persona} </li>`;
-}
-).join(""));
+div_personas.innerHTML = "<ul>" + 
+        personas.map((persona)=>{
+            return `<li> ${persona} </li>`;
+        }).join("");
 
 div_personas.innerHTML += "</ul>";
+
+mostrar_datos(personas, edades, div_personas);
+
+//funciones
+function esMayorDeEdad(edad:number):boolean{
+    return edad >= 18;
+}
+
+function mostrar_datos(nombre:string[], edades:number[], div: HTMLElement){
+    for(let i = 0; i < nombre.length; i++){
+        div.innerHTML += `<p>${nombre[i]} tiene ${edades[i]} años</p>`;
+        if(esMayorDeEdad(edades[i])){
+            div.innerHTML += `<p>${nombre[i]} es mayor de edad</p>`;
+        }else{
+            div.innerHTML += `<p>${nombre[i]} es menor de edad</p>`;
+        }
+    }
+}
