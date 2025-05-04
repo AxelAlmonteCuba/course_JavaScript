3c02c89fb611c03b2c03fb8e55f5a197924ee976
AxelAlmonte <aalmontecu@unsa.edu.pe>
2025-05-03 22:01:59 -0500

    probando clases en typescript

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -1,3 +1,17 @@
+//clases
+var persona = /** @class */ (function () {
+    function persona(nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    persona.prototype.mostrar_datos = function () {
+        return "<p>Hola, soy ".concat(this.nombre, " ").concat(this.apellido, " y tengo ").concat(this.edad, " a\u00F1os.</p>");
+    };
+    return persona;
+}());
+//instanciando la clase
+var persona1 = new persona("Axel", "Almonte", 21);
 //variables
 var nombre = "Axel";
 var apellido = "Almonte";
@@ -5,16 +19,26 @@ var edad = 22;
 //constantes
 var dni = 87654321;
 console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
-//arreglos
-var personas = ["Axel", "Almonte", "Jose"];
-var edades = [22, 11, 24];
 var div_personas = document.querySelector("#personas");
+if (div_personas === null) {
+    throw new Error("No se ha encontrado el elemento con id 'personas'");
+}
+else {
+    div_personas.innerHTML = persona1.mostrar_datos();
+}
+//arreglos
+/*let personas:string[] = ["Axel", "Almonte", "Jose"];
+let edades:number[] = [22, 11, 24];
+
+
 div_personas.innerHTML = "<ul>" +
-    personas.map(function (persona) {
-        return "<li> ".concat(persona, " </li>");
-    }).join("");
+        personas.map((persona)=>{
+            return `<li> ${persona} </li>`;
+        }).join("");
+
 div_personas.innerHTML += "</ul>";
-mostrar_datos(personas, edades, div_personas);
+
+mostrar_datos(personas, edades, div_personas);*/
 //funciones
 function esMayorDeEdad(edad) {
     return edad >= 18;
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,3 +1,26 @@
+//clases
+
+class persona{
+    nombre:string;
+    apellido:string;
+    edad:number;
+
+    constructor(nombre:string, apellido:string, edad:number){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    mostrar_datos():string{
+        return `<p>Hola, soy ${this.nombre} ${this.apellido} y tengo ${this.edad} años.</p>`;
+    }
+}
+
+
+//instanciando la clase
+const persona1:persona = new persona("Axel", "Almonte", 21);
+
+
 //variables
 let nombre:string = "Axel";
 let apellido:string = "Almonte";
@@ -8,11 +31,18 @@ const dni:number = 87654321;
 
 console.log("Hola, soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
 
+let div_personas:HTMLElement | null = document.querySelector("#personas");
+if(div_personas === null){
+    throw new Error("No se ha encontrado el elemento con id 'personas'");
+}else{
+    div_personas.innerHTML = persona1.mostrar_datos();
+}
+
 //arreglos
-let personas:string[] = ["Axel", "Almonte", "Jose"];
+/*let personas:string[] = ["Axel", "Almonte", "Jose"];
 let edades:number[] = [22, 11, 24];
 
-let div_personas:HTMLElement | null = document.querySelector("#personas");
+
 div_personas.innerHTML = "<ul>" + 
         personas.map((persona)=>{
             return `<li> ${persona} </li>`;
@@ -20,7 +50,9 @@ div_personas.innerHTML = "<ul>" +
 
 div_personas.innerHTML += "</ul>";
 
-mostrar_datos(personas, edades, div_personas);
+mostrar_datos(personas, edades, div_personas);*/
+
+
 
 //funciones
 function esMayorDeEdad(edad:number):boolean{
